4f87c64821302db8b51247005b29d614fab17c81
DirkBraakman <119266878+DirkBraakman@users.noreply.github.com>
2025-06-26 01:11:40 +0200

    Delete electron-MQTT-demo directory

--- electron-MQTT-demo/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  receiveConfig: (callback) => ipcRenderer.on('config', callback)
-});
